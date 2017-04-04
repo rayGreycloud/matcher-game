088aca66b0aed433c83a6abba4eab1fa279d5033
rayGreycloud <rayGreycloud@gmail.com>
2017-04-04 12:22:58 -0700

    Add basic grid as board

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -7,7 +7,27 @@ const Dashboard = () => {
     <div>
       <PrivateHeader title="Dashboard" />
       <div className="page-content">
-        <p>Dashboard content goes here</p>
+        <div className="board">
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+          <div className="square">8</div>
+
+          <div className="square">9</div>
+          <div className="square">10</div>
+          <div className="square">11</div>
+          <div className="square">12</div>
+
+          <div className="square">13</div>
+          <div className="square">14</div>
+          <div className="square">15</div>
+          <div className="square">16</div>
+        </div>
       </div>
     </div>
   );
