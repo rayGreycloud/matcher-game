4d3172b5dc2881438344786516c73887c57486ee
rayGreycloud <rayGreycloud@gmail.com>
2017-04-04 12:39:17 -0700

    Add basic styles for dummy board

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -8,25 +8,30 @@ const Dashboard = () => {
       <PrivateHeader title="Dashboard" />
       <div className="page-content">
         <div className="board">
-          <div className="square">1</div>
-          <div className="square">2</div>
-          <div className="square">3</div>
-          <div className="square">4</div>
-
-          <div className="square">5</div>
-          <div className="square">6</div>
-          <div className="square">7</div>
-          <div className="square">8</div>
-
-          <div className="square">9</div>
-          <div className="square">10</div>
-          <div className="square">11</div>
-          <div className="square">12</div>
-
-          <div className="square">13</div>
-          <div className="square">14</div>
-          <div className="square">15</div>
-          <div className="square">16</div>
+          <div className="row">
+            <div className="square">1</div>
+            <div className="square">2</div>
+            <div className="square">3</div>
+            <div className="square">4</div>
+          </div>
+          <div className="row">
+            <div className="square">5</div>
+            <div className="square">6</div>
+            <div className="square">7</div>
+            <div className="square">8</div>
+          </div>
+          <div className="row">
+            <div className="square">9</div>
+            <div className="square">10</div>
+            <div className="square">11</div>
+            <div className="square">12</div>
+          </div>
+          <div className="row">
+            <div className="square">13</div>
+            <div className="square">14</div>
+            <div className="square">15</div>
+            <div className="square">16</div>
+          </div>
         </div>
       </div>
     </div>
